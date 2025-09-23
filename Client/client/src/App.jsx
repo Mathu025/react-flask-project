6a5aba9ca0e43342dfa9dee1200186317d86607d
Mathu025 <mathualex72@gmail.com>
2025-09-23 22:29:00 +0300

    Configure React Router with routes for Home, Trips and My Groups

---
+++ Client/client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Homepage from "./Pages/Homepage";
+import TripsPage from "./Pages/TripsPage";
+import MyGroupsPage from "./Pages/MyGroupsPage";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <div className="container">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/trips" element={<TripsPage />} />
+        <Route path="/my-groups" element={<MyGroupsPage />} />
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
