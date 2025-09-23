d5ce0076fa5dcf93b51021f0fcb14af1b7c908ac
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-09-23 15:19:46 +0300

    Initializing Flask app with SQLAlchemy, migrations and Bcrypt setup

---
+++ Server/models.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+import re
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travelbuddy.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+
+
