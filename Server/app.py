457be53967e2231cc2da11f866e62f1ed8e7c48d
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-09-23 17:13:31 +0300

    Adding welcome and users restful routes

--- Server/app.py
+++ Server/app.py
@@ -10,17 +10,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travelbuddy.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
 api = Api(app)
 
-@app.route('/demo')
-def start():
-    message={
-        "No":"Yes"
-    }
-    response=make_response(message, 200)
-    return response
+
+class Start(Resource):
+    def get(self):
+        response_body={
+            "message":"Welcome to Travel Buddy App"
+        }
+        response=make_response(response_body, 200)
+        return response
+    
+
+
+class UserResource(Resource):
+    def get(self):
+        users=[user.to_dict() for user in User.query.all()]
+        response=make_response(users, 200)
+        return response
+
+    def post(self):
+        data = request.get_json()
+        new_user = User(
+            name=data['name'],
+            email=data['email'],
+            role=data['role']
+        )
+        new_user.password_hash = data['password'] 
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        new_user_dict=new_user.to_dict()
+        response=make_response(new_user_dict, 201)
+        return response
+
+
+api.add_resource(Start, '/welcome')
+api.add_resource(UserResource, '/users')
+
 
 
 
