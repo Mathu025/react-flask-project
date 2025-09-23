30ba0a6b1c478cea8fa1fbb88a709c41ab06b563
Mathu025 <mathualex72@gmail.com>
2025-09-23 22:53:45 +0300

    Add placeholder pages for Home, Trips and My Travel Groups

---
+++ Client/client/src/Pages/MyGroupsPage.jsx
@@ -0,0 +1,8 @@
+function MyGroupsPage() {
+    return (
+        <div>
+            <h1>My Groups Page</h1>
+            <p>This is where you can manage your groups.</p>
+        </div>
+    );
+}
---
+++ Client/client/src/Pages/TripsPage.jsx
@@ -0,0 +1,9 @@
+function TripsPage() {
+    return (
+        <div>
+            <h1>Trips Page</h1>
+            <p>This is where you can view and manage trips.</p>
+        </div>
+    );
+}
+export default TripsPage;   
